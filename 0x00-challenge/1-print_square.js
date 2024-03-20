33fe040dd2be8858dfcd02ea49c7ea8dd0c35e3c
HebaZaki <h_borhan@hotmail.com>
2024-03-20 15:01:24 +0200

    print square

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,11 +1,4 @@
 #!/usr/bin/node
-/*
-    Print a square with the character #
-    
-    The size of the square must be the first argument 
-    of the program.
-*/
-
 
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
@@ -15,6 +8,13 @@ if (process.argv.length <= 2) {
 }
 
 size = parseInt(process.argv[2])
+
+if (isNaN(size) || size <= 0) {
+    // Print error message to stderr
+    process.stderr.write("Invalid size argument: Size must be a positive integer.\n");
+    // Exit with non-zero exit code
+    process.exit(1);
+}
 
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
