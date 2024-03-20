6d2922bc4708b5cd1aad0310591607c57c042e6a
HebaZaki <h_borhan@hotmail.com>
2024-03-20 15:04:18 +0200

    print square

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-if (process.argv.length !==3) {
+if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
@@ -14,8 +14,8 @@ if (isNaN(size) || size <= 0) {
 
 size = parseInt(process.argv[2])
 
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
+for (let i = 0 ; i <= size ; i ++) {
+    for (let j = 0 ; j <= size ; j ++) {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
