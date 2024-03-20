d5109ad4b267e7c205de19c0aee3a59e2399b68a
HebaZaki <h_borhan@hotmail.com>
2024-03-20 15:03:31 +0200

    print square

---
+++ 0x00-challenge/1-print_square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+if (process.argv.length !==3) {
+    process.stderr.write("Missing argument\n");
+    process.stderr.write("Usage: ./1-print_square.js <size>\n");
+    process.stderr.write("Example: ./1-print_square.js 8\n");
+    process.exit(1)
+}
+
+if (isNaN(size) || size <= 0) {
+    process.stderr.write("Invalid size argument: Size must be a positive integer.\n");
+    process.exit(1);
+}
+
+size = parseInt(process.argv[2])
+
+for (let i = 0 ; i < size ; i ++) {
+    for (let j = 0 ; j < size ; j ++) {
+        process.stdout.write("#");
+    }
+    process.stdout.write("\n");
+}
