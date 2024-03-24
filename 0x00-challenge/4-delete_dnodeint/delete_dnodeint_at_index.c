19878e75867b251ae7e2aaa9f49639f31f4b93cf
HebaZaki <h_borhan@hotmail.com>
2024-03-25 01:20:37 +0200

    delete node at index

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,47 +11,34 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *temp;
-	unsigned int i = 0;
+    dlistint_t *current = *head;
+	unsigned int i;
 
-	if (head == NULL || *head == NULL)
-	{
-		return (-1);
-	}
+    if (*head == NULL)
+        return (-1);
 
-	temp = *head;
-	
-	if (index == 0)
-	{
-		*head = (*head)->next;
-		if (*head != NULL)
+    if (index == 0)
+    {
+        *head = (*head)->next;
+        if (*head != NULL)
+            (*head)->prev = NULL;
+        free(current);
+        return (1);
+    }
+	i = 0;
+    while (current != NULL && i < index)
+        current = current->next;
+		i++;
 
-		{
-			(*head)->prev = NULL;
-		}
-		free(temp);
-	}
-	else
-	{
-		while (i < index && temp != NULL)
-		{
-			temp = temp->next;
-			i++;
-		}
-		if (temp == NULL || temp->next == NULL)
-		{
-			return (-1);
-		}
+    if (current == NULL)
+        return (-1);
 
-		if (temp->next != NULL)
-		{
-			temp->next->prev = temp->prev;
-		}
-		if (temp->prev != NULL)
-		{
-			temp->prev->next = temp->next;
-		}
-		free(temp);
-	}
-	return (1);
+    if (current->next != NULL)
+        current->next->prev = current->prev;
+    
+    if (current->prev != NULL)
+        current->prev->next = current->next;
+
+    free(current);
+    return (1);
 }
